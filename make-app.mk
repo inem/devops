70afc00f2fd14498b8eec3b91b0df345dc0d719a
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-24 13:33:23 +0100

    step 30

--- make-app.mk
+++ make-app.mk
@@ -17,3 +17,11 @@ development-setup-env:
 
 app-setup: development-setup-env app-build
 	docker-compose run app bin/setup
+
+ansible-vaults-encrypt:
+	ansible-vault encrypt ansible/development/group_vars/all/vault.yml
+	ansible-vault encrypt ansible/group_vars/all/vault.yml
+
+ansible-vaults-decrypt:
+	ansible-vault decrypt ansible/development/group_vars/all/vault.yml
+	ansible-vault decrypt ansible/group_vars/all/vault.yml
