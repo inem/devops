3ef868c46cd10193e52fc823f753feb22dd89374
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-25 12:44:38 +0100

    ask-password

--- make-production.mk
+++ make-production.mk
@@ -1,5 +1,5 @@
 production-setup:
-	ansible-playbook ansible/site.yml -i ansible/production -u ubuntu -vv
+	ansible-playbook ansible/site.yml -i ansible/production -u ubuntu -vv --ask-vault-pass
 
 production-deploy:
 	ansible-playbook ansible/deploy.yml -i ansible/production -u ubuntu -vv
