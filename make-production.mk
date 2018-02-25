8dd3f1ce82fed0d9441b3c390842e48715a549f7
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-25 19:58:06 +0100

    make docker-image-*

--- make-production.mk
+++ make-production.mk
@@ -3,3 +3,12 @@ production-setup:
 
 production-deploy:
 	ansible-playbook ansible/deploy.yml -i ansible/production -u ubuntu -vv
+
+docker-image-build:
+	docker build -t inemation/workshop-devops-app:v$(V) -f services/app/Dockerfile.production services/app
+
+docker-image-push:
+	docker push inemation/workshop-devops-app:v$(V)
+
+docker-image-run:
+	docker run -it inemation/workshop-devops-app:$(V) bash
